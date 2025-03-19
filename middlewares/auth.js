b435b734cb65ce2fa74dfa4b73756d0c1204867f
Emadenni <emanyele.dng86@gmail.com>
2025-03-19 21:51:15 +0100

    corrected jwt secret variable

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,7 +11,7 @@ export const auth = () => {
         }
 
         const token = authHeader.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
         request.event.user = decoded;
       } catch (error) {
