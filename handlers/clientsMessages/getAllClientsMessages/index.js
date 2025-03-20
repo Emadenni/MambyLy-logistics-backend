746908fbccabeee4ec5c8684d06c1d1af3810f7c
Emadenni <emanyele.dng86@gmail.com>
2025-03-20 11:18:27 +0100

    changed getAllMessages responses

--- handlers/clientsMessages/getAllClientsMessages/index.js
+++ handlers/clientsMessages/getAllClientsMessages/index.js
@@ -3,13 +3,17 @@ import { sendSuccessResponse, sendError } from "../../../responses/index.js";
 import { getAllClientsMessages } from "../../../services/clientsMessagesService.js";
 
 const getAllClientsMessagesHandler = async () => {
-    try {
-      const messages = await getAllClientsMessages();
-  
-      return sendSuccessResponse(200, messages);
-    } catch (error) {
-      return sendError(500, error.message || "Internal server error");
+  try {
+    const messages = await getAllClientsMessages();
+
+    if (!messages || messages.length === 0) {
+      return sendSuccessResponse(200, { message: "No messages found" });
     }
-  };
-  
-  export const handler = middy(getAllClientsMessagesHandler);
+
+    return sendSuccessResponse(200, messages);
+  } catch (error) {
+    return sendError(500, error.message || "Internal server error");
+  }
+};
+
+export const handler = middy(getAllClientsMessagesHandler);
--- handlers/jobMessages/getAllJobMessages/index.js
+++ handlers/jobMessages/getAllJobMessages/index.js
@@ -6,6 +6,10 @@ const getAllJobMessagesHandler = async () => {
   try {
     const messages = await getAllJobMessages();
 
+    if (!messages || messages.length === 0) {
+      return sendSuccessResponse(200, { message: "No messages found" });
+    }
+
     return sendSuccessResponse(200, messages);
   } catch (error) {
     return sendError(500, error.message || "Internal server error");
