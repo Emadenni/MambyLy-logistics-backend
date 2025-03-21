6e94ab0adfce58c5412ede1a20f79d516efb07c0
Emadenni <emanyele.dng86@gmail.com>
2025-03-21 23:28:46 +0100

    improved errors catching

--- handlers/admins/registerAdmin/index.js
+++ handlers/admins/registerAdmin/index.js
@@ -1,10 +1,3 @@
-import middy from "@middy/core";
-import { registerAdmin } from "../../../services/adminServices.js";
-import { sendSuccessResponse, sendError } from "../../../responses/index.js";
-import { validation } from "../../../middlewares/validation.js";
-import { adminSchema } from "../../../utils/adminSchema.js";
-import { auth } from "../../../middlewares/auth.js";
-
 const registerAdminHandler = async (event) => {
   try {
     const adminData = event.body;
@@ -15,6 +8,12 @@ const registerAdminHandler = async (event) => {
       admin,
     });
   } catch (error) {
+    
+    if (error.message === "Email Already in use") {
+      return sendError(400, error.message);  
+    }
+
+   
     return sendError(500, error.message || "Internal server error");
   }
 };
