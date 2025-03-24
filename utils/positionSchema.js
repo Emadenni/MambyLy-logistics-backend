331d50299e59bdee1cff7415ea9e5d8df2cf8a90
Emadenni <emanyele.dng86@gmail.com>
2025-03-24 09:44:56 +0100

    corrected departure attribute schema

--- utils/positionSchema.js
+++ utils/positionSchema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const positionSchema = Joi.object({
   positionId: Joi.string().min(2).max(20).required(),
-  departure: Joi.string().email().max(20).required(),
+  departure: Joi.string().min(2).max(20).required(),
   destination: Joi.string().min(3).max(20).required(),
   distance: Joi.string().min(10).max(10).required(),
   type: Joi.string().min(10).max(30).required(),
