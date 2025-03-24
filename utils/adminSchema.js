745643bb279adcda0446ff4792a7efe5e5cf867f
Emadenni <emanyele.dng86@gmail.com>
2025-03-24 22:16:03 +0100

    added extra validation for admin password

--- utils/adminSchema.js
+++ utils/adminSchema.js
@@ -4,7 +4,13 @@ export const adminSchema = Joi.object({
   firstName: Joi.string().min(2).max(50).required(),
   lastName: Joi.string().min(2).max(50).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string()
+    .min(6)  // Minimum 6 characters
+    .pattern(new RegExp('^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*]).{6,}$'))  
+    .required()
+    .messages({
+      'string.pattern.base': 'Password must contain at least one letter, one number, and one special character (!@#$%^&*).',
+    }),
   profileImage: Joi.string().uri().optional(),
 });
 
