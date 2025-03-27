ac712f4cfd6ebd19eb0216e9de3e450d58d923cb
Emadenni <emanyele.dng86@gmail.com>
2025-03-27 10:59:32 +0100

    removed password from udpateAdminSchema

--- utils/adminSchema.js
+++ utils/adminSchema.js
@@ -23,6 +23,5 @@ export const updateAdminSchema = Joi.object({
   firstName: Joi.string().min(2).max(50).optional(),
   lastName: Joi.string().min(2).max(50).optional(),
   email: Joi.string().email().optional(),
-  password: Joi.string().min(6).optional(),
   profileImage: Joi.string().uri().optional(), 
 });
